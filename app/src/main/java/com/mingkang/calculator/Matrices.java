a3e81aa6684ab80347133d3635737247341b7c29
khawlinkoon <khawlinkoon@gmail.com>
2020-05-13 14:07:58 +0800

    Updated matrix (Incomplete) Part 2

--- app/src/main/java/com/mingkang/calculator/Matrices.java
+++ app/src/main/java/com/mingkang/calculator/Matrices.java
@@ -5,7 +5,6 @@ import androidx.gridlayout.widget.GridLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +23,7 @@ public class Matrices extends AppCompatActivity implements View.OnClickListener 
     private double[] matricA, matricB, matricAns;
     private boolean matrices_currentState = true; // true for 2d, false for 3d
     private boolean matrices_currentMatric = true; // true for A, false for B
+    private int matrices_ansSize;
     private ArrayList<String> matrices_equation;
     private String matric_final;
 
@@ -78,7 +78,7 @@ public class Matrices extends AppCompatActivity implements View.OnClickListener 
 
     private void matrices_initialiseBtn(){
         findViewById(R.id.matric_btnA).setOnClickListener(Matrices.this);
-        findViewById(R.id.matric_btnAnswer).setOnClickListener(Matrices.this);
+        findViewById(R.id.matric_btnRightBracket).setOnClickListener(Matrices.this);
         findViewById(R.id.matric_btnB).setOnClickListener(Matrices.this);
         findViewById(R.id.matric_btnCalculate).setOnClickListener(Matrices.this);
         findViewById(R.id.matric_btnClear).setOnClickListener(Matrices.this);
@@ -135,14 +135,22 @@ public class Matrices extends AppCompatActivity implements View.OnClickListener 
             break;
 
             case "calculate" :
-//                if(matric_validate()){
-//
-//                }
+                if(matric_validate()){
+                    Toast.makeText(Matrices.this, matric_simpilfy(), Toast.LENGTH_LONG).show();
+                    //matric_calculate();
+                }
 
             break;
 
             case "del" :
                 if(matrices_equation.size()>0){
+//                    if(matrices_equation.get(matrices_equation.size()-1).equals(")")){
+//                        matrices_bracket++;
+//                    }
+//                    if(matrices_bracket>0 && !matrices_equation.get(matrices_equation.size()-1).equals(")")){
+//                        matrices_equation.add(")");
+//                        matrices_bracket--;
+//                    }
                     matrices_equation.remove(matrices_equation.size()-1);
                     matric_showEquation();
                 }
@@ -157,18 +165,151 @@ public class Matrices extends AppCompatActivity implements View.OnClickListener 
             default :
                 String matric_input = v.getTag().toString();
                 matrices_equation.add(matric_input);
-
+//                if(matric_input.equals("DET(") || matric_input.equals("TRN(")){
+//                    matrices_bracket++;
+//                }
+//                else if(matrices_bracket>0){
+//                    matrices_equation.add(")");
+//                    matrices_bracket--;
+//                }
                 matric_showEquation();
 
             break;
         }
     }
 
-//    private boolean matric_validate(){ // Remember to add error message here <--
-//        if(){
-//
-//        }
-//    }
+    private boolean matric_validate(){ // Remember to add error message here <--
+        if(matrices_equation.size() == 0){
+            Toast.makeText(Matrices.this, "Equation cannot be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!matric_checkBracket()){
+            Toast.makeText(Matrices.this, "Wrong bracket location", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!matric_checkOperator()){
+            Toast.makeText(Matrices.this, "Cannot have simultaneous operators", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!matric_checkInverse()){
+            Toast.makeText(Matrices.this, "Inverse must be in front of a matrix", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!matric_checkLastOp()){
+            Toast.makeText(Matrices.this, "Invalid equation", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private String matric_simpilfy(){
+        String fa = "";
+        ArrayList<String> temp = new ArrayList<>();
+        for(int i=0;i<matrices_equation.size();i++){
+            String temp1 = matrices_equation.get(i);
+            if(i==0){
+                temp.add(temp1);
+            }
+            else if(temp.get(temp.size()-1) == "DET(" && temp1 == "DET("){
+                temp.remove(temp.size()-1);
+            }
+            else if(temp.get(temp.size()-1) == "TRN(" && temp1 == "TRN("){
+                temp.remove(temp.size()-1);
+            }
+            else if(temp.get(temp.size()-1) == "⁻¹" && temp1 == "⁻¹"){
+                temp.remove(temp.size()-1);
+            }
+            else{
+                temp.add(temp1);
+            }
+        }
+        for(int i=0;i<temp.size();i++) fa += temp.get(i);
+        return fa;
+    }
+
+    private void matric_calculate(){
+        ArrayList<String> temp = new ArrayList<>();
+        String temp1 = "";
+        boolean check = false;
+        for(int i=matrices_equation.size()-1;i>=0;i--){
+            if(check){
+
+            }
+            if(matrices_equation.get(i).equals(")")){
+                check = true;
+            }
+        }
+    }
+
+    private boolean matric_checkLastOp(){
+        boolean check = true, check2 = false;
+        for(int i=0;i<matrices_equation.size();i++){
+            if(matrices_equation.get(i).equals("A") || matrices_equation.get(i).equals("B")
+                    || matrices_equation.get(i).equals("⁻¹"))
+                check=false;
+            if((matrices_equation.get(i).equals("+") || matrices_equation.get(i).equals("-")
+                    || matrices_equation.get(i).equals("*"))
+                    && check){
+                return false;
+            }
+            else if((matrices_equation.get(i).equals("+") || matrices_equation.get(i).equals("-")
+                    || matrices_equation.get(i).equals("*"))
+                    && !check){
+                check2 = true;
+                check = true;
+            }
+            if((matrices_equation.get(i).equals(")") || matrices_equation.get(i).equals("DET(")) && check2)
+                return false;
+            else if((matrices_equation.get(i).equals(")") || matrices_equation.get(i).equals("DET(")) && !check2){
+                check2 = false;
+            }
+        }
+        if(matrices_equation.get(matrices_equation.size()-1).equals("+")
+                || matrices_equation.get(matrices_equation.size() - 1).equals("-")
+                || matrices_equation.get(matrices_equation.size() - 1).equals("*")
+                || matrices_equation.get(matrices_equation.size() - 1).equals("DET(")
+                || matrices_equation.get(matrices_equation.size() - 1).equals("TRN(")) return false;
+        return true;
+    }
+
+    private boolean matric_checkInverse(){
+        for(int i=1;i<matrices_equation.size();i++){
+            if(matrices_equation.get(i) == "⁻¹"){
+                if(matrices_equation.get(i-1) != "A" && matrices_equation.get(i-1) != "B"){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean matric_checkOperator(){
+        boolean op = false;
+        for(int i=0;i<matrices_equation.size();i++){
+            if(matrices_equation.get(i).equals("+") || matrices_equation.get(i).equals("-")
+                    || matrices_equation.get(i).equals("*")){
+                if(op) return false;
+                else op = true;
+            }
+            else op = false;
+        }
+        return true;
+    }
+
+    private boolean matric_checkBracket(){
+        int bs=0, be=0;
+        for(int i=0;i<matrices_equation.size();i++){
+            if(matrices_equation.get(i).equals("DET(") || matrices_equation.get(i).equals("TRN(")) bs++;
+            if(matrices_equation.get(i).equals(")")) be++;
+        }
+        boolean check = false;
+        for(int i=0;i<matrices_equation.size();i++){
+            if(matrices_equation.get(i).equals("DET(") || matrices_equation.get(i).equals("TRN(")) check = true;
+            if(matrices_equation.get(i).equals(")") && check) check = false;
+            else if(matrices_equation.get(i).equals(")") && !check) return false;
+        }
+        return bs == be;
+    }
 
     private void matric_showEquation(){
         TextView matric_equation = findViewById(R.id.matric_equationview);
